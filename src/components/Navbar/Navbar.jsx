963ff0230a80bb7df3fd47383dc7b054df811426
Laklak <nareglaklakian@gmail.com>
2022-08-13 13:36:36 +0400

    commit 1a

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+// import React from 'react';
+// import github from '../../img/github.png';
+// import linkedin from '../../img/linkedin.png';
+// import { Link } from 'react-scroll';
+
+// function Navbar() {
+//   return (
+//     <nav className="nav">
+//       <a href="/" className="site-title">
+//         Laklak
+//       </a>
+//       <text className="text"> | </text>
+//       <a href="/projects">
+//         <Link
+//           className="content-projects"
+//           to="content-projects"
+//           smooth={true}
+//           duration={500}
+//         ></Link>{' '}
+//         Projects
+//       </a>
+//       <a href="https://github.com/nareg-laklakian" className="github">
+//         {' '}
+//         {/* <img src={github} alt="github" width={15} /> */}
+//         github
+//       </a>
+//       <a
+//         href="https://www.linkedin.com/in/nareg-laklakian-b10120200/"
+//         className="linkedIn"
+//       >
+//         {' '}
+//         {/* <img src={linkedin} alt="linkedin" width={15} /> */}
+//         linkedIn
+//       </a>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
