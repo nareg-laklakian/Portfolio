c05a828738227d5d2c3b157335ba56a26d99cafc
Laklak <nareglaklakian@gmail.com>
2022-08-19 15:15:16 +0400

    commit 6a

--- src/components/Navbar/Projects.jsx
+++ src/components/Navbar/Projects.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import calculator from '../../img/calculator.PNG';
 import pool from '../../img/pool-game.PNG';
+import dice from '../../img/dice-high-number-game.PNG';
+import guess from '../../img/guess-number-game.PNG';
 
 function Projects() {
   return (
@@ -10,11 +12,28 @@ function Projects() {
         <h1>Projects</h1>
       </div>
       <div className="projects">
-        <div className="calculator-image">
+        <div className="first-row-space">
+          <div className="calculator-image">
+            <img src={calculator} alt="calculator" />
+          </div>
+          <div className="pool-image">
+            <img src={pool} alt="pool game" />
+          </div>
+        </div>
+        {/* <div className="calculator-image">
           <img src={calculator} alt="calculator" />
         </div>
         <div className="pool-game-image">
           <img src={pool} alt="pool game" />
+        </div> */}
+
+        <div className="second-row-space">
+          <div className="dice-image">
+            <img src={dice} alt="dice game" />
+          </div>
+          <div className="guess-number-image">
+            <img src={guess} alt="guess-number-game" />
+          </div>
         </div>
       </div>
     </div>
